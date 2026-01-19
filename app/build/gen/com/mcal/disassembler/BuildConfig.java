f94af9c2b2dd1ea8f834b8a49f02c85a07f579c8
FlutterGenerator <elementman50@yandex.com>
2026-01-19 10:26:50 +0500

    update

---
+++ app/build/gen/com/mcal/disassembler/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+* Automatically generated file. DO NOT MODIFY
+*/
+package com.mcal.disassembler;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = false;
+    public static final String APPLICATION_ID = "com.mcal.disassembler";
+    public static final String BUILD_TYPE = "release";
+    public static final int VERSION_CODE = 38;
+    public static final String VERSION_NAME = "3.8";
+}
