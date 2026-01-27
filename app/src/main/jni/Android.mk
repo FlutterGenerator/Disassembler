f0dcaa9da2618e67ecb2e1c336fcfc54efa79b52
tojik_proof_93 <elementman50@yandex.com>
2026-01-27 23:54:08 +0500

    Update Android.mk

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -5,6 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_CPP_EXTENSION := .cpp .cc
 LOCAL_MODULE    := disassembler
 LOCAL_SRC_FILES := Disassembler.cpp
+LOCAL_LDFLAGS += "-Wl,-z,max-page-size=16384"
 
 #ifeq ($(TARGET_ARCH_ABI),x86)
 #   LOCAL_CFLAGS += -ffast-math -mtune=atom -mssse3 -mfpmath=sse
