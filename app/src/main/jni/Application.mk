9c6d212d94f2a742ed8b10dfbeb23135da4ec915
tojik_proof_93 <elementman50@yandex.com>
2026-02-12 08:11:18 +0500

    fix

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,6 +1,6 @@
 APP_STL := c++_static
 APP_ABI := all
-APP_PLATFORM := android-21
+# APP_PLATFORM := android-21
 APP_CFLAGS := -O2 -std=c++14
 APP_CPPFLAGS += -std=c++14
 APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true
